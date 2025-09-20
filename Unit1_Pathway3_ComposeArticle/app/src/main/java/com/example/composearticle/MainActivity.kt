cbf8f247d7c897218be0dce3eb1706d24b7479b5
H.Son <sorrylight1@gmail.com>
2025-09-20 16:33:49 +0700

    Feat: Add ComposeArticle
    Using verticalScroll(rememberScrollState) to help user read the full version of longDes

---
+++ Unit1_Pathway3_ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.composearticle
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composearticle.ui.theme.ComposeArticleTheme
+import kotlinx.coroutines.internal.OpDescriptor
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeArticleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticleApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.title),
+        shortDes = stringResource(R.string.short_desc),
+        longDes = stringResource(R.string.long_desc),
+
+        imageP = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ComposeArticleAppPreview () {
+    ComposeArticleTheme {
+        ComposeArticleApp()
+    }
+}
+
+@Composable
+fun ArticleCard(
+    title : String,
+    shortDes: String,
+    longDes: String,
+    imageP: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) {
+        Image(painter = imageP, contentDescription = null)
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDes,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDes,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 16.dp, bottom = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
